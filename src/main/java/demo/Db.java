2f83851f99ff5e1dedbca9c0f0389351d484e753
Ali Ahmed <aliahmedx8@gmail.com>
2025-10-23 00:23:57 -0400

    Assignment 2 – Complete deployment with Tomcat + Apache + MySQL

--- src/main/java/demo/Db.java
+++ src/main/java/demo/Db.java
@@ -5,11 +5,12 @@ public final class Db {
     try { Class.forName("com.mysql.cj.jdbc.Driver"); } catch (Exception ignore) {}
   }
 
+  // ✅ Level 3: Laptop → Server EC2 (Public IP, No SSL)
   private static final String URL =
-      "jdbc:mysql://192.168.1.183:3306/se"
+      "jdbc:mysql://18.189.21.239:3306/SE"
     + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC&characterEncoding=UTF-8";
-  private static final String USER = "se_app";
-  private static final String PASS = "se_app_123";
+  private static final String USER = "SE";
+  private static final String PASS = "SE2020";
 
   public static java.sql.Connection getConnection() throws java.sql.SQLException {
     return java.sql.DriverManager.getConnection(URL, USER, PASS);
